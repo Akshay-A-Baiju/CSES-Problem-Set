c585cc519812155ecc8c9dcd84016758c06c1189
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-07-29 22:59:35 +0530

    Add files via upload
    #STL
    #next_permutation

---
+++ Introductory Problems/15_Creating Strings_Method1_STL.cpp
@@ -0,0 +1,62 @@
+// Author : Akshay A Baiju
+// Problem link : https://cses.fi/problemset/task/1622/
+// Problem tag : STL, next_permutation
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll                  long long
+#define ull                 unsigned long long
+#define vi                  vector <int>
+#define vll                 vector <ll>
+#define vvll                vector <vll>
+#define pii                 pair <int,int>
+#define pll                 pair <ll,ll>
+#define ms(a,x)             memset (a,x,sizeof(a))
+#define rep(i,a,n)          for (int i=a;i<n;i++)
+#define rrep(i,n,a)         for (int i=n;i>=a;i--)
+#define pb                  push_back
+#define pf                  push_front
+#define ppb                 pop_back
+#define umap                unordered_map
+#define uset                unordered_set
+#define all(v)              v.begin(), v.end()
+#define desc()              greater <int>()
+#define endl                "\n"   //not to be used in interactive problems
+#define scan_vector(v,n)    for (auto &it: v) { cin>>it; }
+#define print_vector(v,n)   for (auto &it: v) { cout<<it<<" "; } cout<<endl;
+#define print_map(mp)       for (auto &pr: mp) { cout<<pr.first<<" "<<pr.second<<endl; }
+#define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const int M = 1e9+7;
+
+void solve()
+{
+    //using STL function : next_permutation, we can directly find all the permutations
+     
+    string s;
+    cin>>s;
+    sort (all(s));
+    vector <string> perm;
+    do
+    {
+        perm.pb(s);
+    }
+    while (next_permutation(all(s)));
+    cout<<perm.size()<<endl;
+    for (auto &it: perm)
+        cout<<it<<endl;
+}
+
+int main()
+{
+    clock_t clk = clock();
+    sync;
+    int t=1;
+    //cin>>t;
+    while (t--)
+    {
+        solve();
+        cout<<endl;
+    }
+    cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    return 0;
+}
