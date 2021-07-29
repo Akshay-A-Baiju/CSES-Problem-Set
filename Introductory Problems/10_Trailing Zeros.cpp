0ed9e9da545c2926ff7877a15d93ba8e0c394a1b
Akshay A Baiju <79350637+Akshay-A-Baiju@users.noreply.github.com>
2021-07-29 13:12:36 +0530

    Add files via upload
    #logical
    #must know

---
+++ Introductory Problems/10_Trailing Zeros.cpp
@@ -0,0 +1,60 @@
+// Author : Akshay A Baiju
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll                  long long
+#define ull                 unsigned long long
+#define vi                  vector <int>
+#define vll                 vector <ll>
+#define vvll                vector <vll>
+#define pii                 pair <int,int>
+#define pll                 pair <ll,ll>
+#define ms(a,x)             memset (a,x,sizeof(a))
+#define rep(i,a,n)          for (int i=a;i<n;i++)
+#define rrep(i,n,a)         for (int i=n;i>=a;i--)
+#define pb                  push_back
+#define pf                  push_front
+#define ppb                 pop_back
+#define umap                unordered_map
+#define uset                unordered_set
+#define all(v)              v.begin(), v.end()
+#define desc()              greater <int>()
+#define endl                "\n"   //not to be used in interactive problems
+#define scan_vector(v,n)    for (auto &it: v) { cin>>it; }
+#define print_vector(v,n)   for (auto &it: v) { cout<<it<<" "; } cout<<endl;
+#define print_map(mp)       for (auto &pr: mp) { cout<<pr.first<<" "<<pr.second<<endl; }
+#define sync                ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+const int M = 1e9+7;
+
+ll trailingZeros(ll n)
+{
+    //TC: O(log n)
+    ll zeros=0;
+    for (ll i=5;i<=n;i=i*5)
+        zeros=zeros + (n/i);
+    return zeros;
+}
+
+void solve()
+{
+    //Trailing zeros count of fact(n)=floor(n/5)+ floor(n/25)+ floor(n/125)+ ... , for n
+
+    ll n;
+    cin>>n;
+    cout<<trailingZeros(n);
+}
+
+int main()
+{
+    clock_t clk = clock();
+    sync;
+    int t=1;
+    //cin>>t;
+    while (t--)
+    {
+        solve();
+        cout<<endl;
+    }
+    cerr << '\n'<<"Time (in s): " << double(clock() - clk) * 1.0 / CLOCKS_PER_SEC << '\n';
+    return 0;
+}
